39d35786734b4b5f604204f689110563c9899e84
benjamin_sanchez <benjaminjosesanchez4@gmail.com>
2025-08-02 03:59:31 -0300

    ojala

--- src/server.js
+++ src/server.js
@@ -1,13 +1,25 @@
 const express = require('express')
 const path = require('path')
-
+const cors = require('cors')
 // Inicialización de la app
 const app = express()
 // Settings
 app.set('port', process.env.PORT || 3000)
 // Middlewares
 app.use(express.urlencoded({ extended: false }))
-// Variables
+app.use(express.json())
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: [
+    'Origin',
+    'X-Requested-With',
+    'Content-Type',
+    'Accept',
+    'Access-Control-Allow-Request-Method',
+    'KEY'
+  ]
+}))
 // Rutas
 app.use(require('./rutas/Noticias-rutas'))
 // archivo estático
