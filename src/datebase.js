67126d38615432905d122440541d6a38c4a32bb4
benjamin_sanchez <benjaminjosesanchez4@gmail.com>
2025-08-02 04:14:39 -0300

    ojala 3

--- src/datebase.js
+++ src/datebase.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose')
 const mongoUrl = process.env.mongoUrl
 mongoose
   .connect(mongoUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+
   })
   .then((db) => console.log('Database is connected'))
   .catch((err) => console.error(err))
